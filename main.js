8d5af3b4afada8e187a64148993fb84e9c21f276
Jakob Sturla <jseinarsson@gmail.com>
2020-10-10 21:22:08 +0000

    Success message added, JS completed, README added, finito

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Create variable to target form
+const form = document.querySelector(".contact__form");
+
+// Create an array with custom messages for each part of the form
+// The setup is [formid]Message so that we can call on the correct message for each
 const customMessages = {
     nameMessage: 'Please tell us what to call you',
     emailMessage: 'Please provide a valid e-mail address',
@@ -6,27 +11,37 @@ const customMessages = {
     checkboxMessage: 'Say it! Say tomatoes are a fruit.',
 }
 
+// Variables that will be used later to add classes to the input container (li) and error container (a paragraph element created with JS)
 const validationErrorClass = 'error'
-const parentErrorClass = 'has-error'
+const parentErrorClass = 'invalid'
+
+// Create a variable targeting all input types used in the form
 const inputs = document.querySelectorAll('input, select, textarea');
 
+// A function that runs through each of the inputs targeted with the variable above, executing the nested functions
 inputs.forEach(function (input) {
 
+    //
     function checkValidity (options) {
         const insertError = options.insertError;
+        // Creates a variable called parent from the input's container
         const parent = input.parentNode;
+        // Makes the variable error equal to an element within the parent with the query selector ".invalid" OR creates a new paragraph element
         const error = parent.querySelector(`.${validationErrorClass}`)
             || document.createElement('p');
         
-        if (!input.validity.valdi && input.validationMessage) {
+        if (!input.validity.valid) {
             error.className = validationErrorClass;
             error.textContent = customMessages[`${input.id}Message`];
-
+            //If the input is not valid this adds the class "input" to the error paragraph and passes the correct message from the array defined above
+            
             if (insertError) {
+                // If insertError (see below) is true, this adds the error to the end of the parent container and gives the parent container a class of "invalid"
                 parent.appendChild(error);
                 parent.classList.add(parentErrorClass);
             }
         } else {
+            // Otherwise it removes the error and the parent's invalid class
             parent.classList.remove(parentErrorClass);
             error.remove();
         }
@@ -37,7 +52,17 @@ inputs.forEach(function (input) {
     });
 
     input.addEventListener('invalid', function (e) {
+        // If the input is invalid, this sends the insert error true to the checkValidity function above and preventDefault stops the browser from throwing the inbuilt error messages
         e.preventDefault();
         checkValidity({insertError: true});
     });
+});
+
+// When all errors have been resolved and the form is submitted, this function prevents the default reloading of the page and instead hides the form and shows a success message
+form.addEventListener('submit', function (e) {
+    const successMsg = document.querySelector(".contact__success");
+
+    e.preventDefault();
+    form.classList.toggle("hidden");
+    successMsg.classList.toggle("hidden");
 });
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Module 3: Javascript In The Browser - Forms
+
+This is a return for the forms validation assignment.
+The project can be seen live at [fruity-tomatoes.surge.sh](http://fruity-tomatoes.surge.sh)
+
+There are 5 different types of input:
+1. Text
+2. E-mail
+3. Selection with different options
+4. A long textarea
+5. A checkbox
+
+All inputs have a HTML5 **required** attribute.
+The Javascript overrides the default browser validation messages with in-browser styled error messages and adds a red outline to missed inputs.
+When the form is fully completed, a success message is displayed.
+
+The Javascript is explained with comments (no explanations in HTML and CSS files).
